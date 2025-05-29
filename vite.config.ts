9d204c2ac43bd7ac104771cfde244db3d940a403
guohub8080 <guo2018@88.com>
2025-05-30 00:43:38 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -73,7 +73,7 @@ export default defineConfig({
 		extensions: [".ts", ".tsx", ".js", "jsx"]
 	},
 	publicDir: resolve(__dirname, 'public'), // 绝对路径确保准确性
-	base: '/',
+	base: './',
 	build: {
 		outDir: "docs",
 		copyPublicDir: true,
